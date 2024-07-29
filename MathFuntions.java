aee64521c85fa1a47f9e97670e514de67a912ac6
Muhammadcoder2001 <abduqahhorovmuhammadqobil@gmail.com>
2024-07-29 13:52:37 +0500

    Method overloading

---
+++ MathFuntions.java
@@ -0,0 +1,22 @@
+package OOP_method_overlaoding;
+
+public class MathFuntions {
+
+    public  void add(int a, int b){
+        System.out.println(a + b);
+    }
+
+    public void substarct(int a , int b){
+        System.out.println(a - b);
+    }
+
+    public  void multiply(int a, int b){
+        System.out.println(a * b);
+    }
+
+    public void divide(int a , int b){
+        System.out.println( a / b);
+    }
+
+
+}
